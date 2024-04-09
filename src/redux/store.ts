97e09ed8d8cb6bb1b0cba81da5b710beabc4601e
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2024-04-09 14:30:30 +0300

    add redux-persist, save in localStorage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,48 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import issueReducer from "./issue/issueSlice";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
-export type RootState = ReturnType<typeof store.getState>;
+// export const store = configureStore({
+//   reducer: {
+//     issues: issueReducer,
+//   },
+// });
+
+const rootReducer = combineReducers({
+  issues: issueReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    issues: issueReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
 
 export const useAppDispatch: () => typeof store.dispatch = useDispatch;
 export const appSelector: TypedUseSelectorHook<
